995ad36aca2821f1ecaef46a971edcf283079b7a
Marco Haiat <marco.haiat@gmail.com>
2023-05-16 15:08:13 -0300

    feat: req 07 approved - Category model, it was almost finished already

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,7 +3,7 @@
 const CategoriesModel = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    displayName: DataTypes.STRING,
+    name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
