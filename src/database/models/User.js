05811976d3afab0cae1f7ae9d3ca2f7ea9ae1f6e
Marco Haiat <marco.haiat@gmail.com>
2023-05-28 11:02:09 -0300

    feat: initial test with class in user model

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,22 +1,49 @@
 // src/models/user.model.js
 
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  },
-  {
-    underscored: true,
-    timestamps: false,
-  });
+// class userModel {
+//   static associate(models) {
+//     User.hasMany(models.BlogPost, { foreignKey: true, as: 'blog_posts' });    
+//   }
+
+//   constructor(sequelize, DataTypes) {
+//     return sequelize.define(
+//       'User',
+//       {
+//         id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//         displayName: DataTypes.STRING,
+//         email: DataTypes.STRING,
+//         password: DataTypes.STRING,
+//         image: DataTypes.STRING,
+//       },
+//       {
+//         underscored: true,
+//         timestamps: false,
+//       }
+//     );
+//   }
+// }
+
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      underscored: true,
+      timestamps: false,
+    }
+  );
 
   User.associate = (models) => {
-    User.hasMany(models.BlogPost, 
-      { foreignKey: true, as: 'blog_posts'});
+    User.hasMany(models.BlogPost, { foreignKey: true, as: 'blog_posts' });
   };
 
   return User;
 };
+
+module.exports = userModel;
